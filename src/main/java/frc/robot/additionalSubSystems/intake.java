875c23031faa21ef421da7e3d2cc8dbe6094dc98
Team4286 <robotics2257@gmail.com>
2026-03-02 16:33:47 -0500

    Intake temp

--- src/main/java/frc/robot/additionalSubSystems/intake.java
+++ src/main/java/frc/robot/additionalSubSystems/intake.java
@@ -2,4 +2,18 @@ package frc.robot.additionalSubSystems;
 // sets up an intake 
 public class intake {
     
+    //put variables here two motors, one for the roller and one for the arm
+    //
+
+    public intake(){
+        //initialize motors here
+    }
+    //conner is gay
+     //put methods here, one to run the roller and one to move the arm
+     public void runRoller(double speed){
+        //set roller motor to speed
+     }
+     public void moveArm(double position){
+        //set arm motor to position
+     }
 }
